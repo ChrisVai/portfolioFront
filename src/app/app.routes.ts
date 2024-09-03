30dc9622457727860dc3163ea647efd84885ace8
ChrisVai <chrisbellet@gmail.com>
2024-09-03 16:25:10 +0200

    ajout route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {AboutMeComponent} from "./features/aboutMe/components/about-me/about-me.component";
+import {HomeComponent} from "./features/home/components/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent, pathMatch: "full"},
+  {path: 'aboutme', component: AboutMeComponent, pathMatch: "full"}
+];
