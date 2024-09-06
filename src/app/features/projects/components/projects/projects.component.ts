f04f18e15a8bf3860fad0f42655e24048f85d109
ChrisVai <chrisbellet@gmail.com>
2024-09-06 17:54:03 +0200

    update projects component

--- src/app/features/projects/components/projects/projects.component.ts
+++ src/app/features/projects/components/projects/projects.component.ts
@@ -17,13 +17,12 @@ import {ProjectCardComponent} from "../project-card/project-card.component";
 export class ProjectsComponent {
 
   private yushoot: ProjectModel = {
-    imgUrl: ["projectAssets/yushoot/yushootDesktop.png"],
+    imgUrl: ["projectAssets/yushoot/yushootDesktop.png", "projectAssets/yushoot/FullPage(iPad Pro).png", "projectAssets/yushoot/FullPageZoom(iPhone 12 Pro).png"],
     name: "Yushoot",
     shortDescription: "Refonte de le page Profil photographe de Yushoot.fr",
-    fullDescription: "Refonte complète en mobile First de la page Profil Photographe du site Yushoot.fr. Amélioration de l'UX/UI.",
-    frontTechno: ["Angular 17", "TypeScript", "SaSS & SCSS", "GraphQL / Apollo"],
-    backTechno: ["NodeJs", "Express", "GraphQL"],
-    bddTechno: ["MongoDB", "Docker"],
+    fullDescription: "Refonte complète en mobile First de la page Profil Photographe du site Yushoot.fr. Amélioration de l'UX/UI." +
+      "Création d'Endpoint.",
+    technologies: ["Angular 17", "TypeScript", "SaSS & SCSS", "GraphQL / Apollo","NodeJs", "Express", "GraphQL","MongoDB", "Docker"],
   }
 
   projects: ProjectModel[] = [this.yushoot]
