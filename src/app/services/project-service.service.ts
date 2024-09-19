f1832f1813fa0efdf947cca637129724e0f2a06f
ChrisVai <chrisbellet@gmail.com>
2024-09-19 18:31:38 +0200

    corrections

--- src/app/services/project-service.service.ts
+++ src/app/services/project-service.service.ts
@@ -10,7 +10,7 @@ export class ProjectService {
     imgUrl: ["projectAssets/yushoot/yushootDesktop.png", "projectAssets/yushoot/FullPage(iPad Pro).png", "projectAssets/yushoot/FullPageZoom(iPhone 12 Pro).png"],
     name: "Yushoot",
     shortDescription: "Refonte de le page Profil photographe de Yushoot.fr",
-    fullDescription: "Refonte complète en mobile First de la page Profil Photographe du site Yushoot.fr. Amélioration de l'UX/UI et optimisation des performances. " +
+    fullDescription: "Refonte complète en mobile First de la page Profil Photographe du site Yushoot.fr. Amélioration de l'UX/UI et optimisation des performances via RxJS et ChangeDetectionStrategy.OnPush.<br>" +
       "Création d'Endpoint sur l'API GraphQL.",
     technologies: ["Angular 17", "TypeScript", "SaSS & SCSS", "RxjS", "GraphQL / Apollo","NodeJs", "Express", "GraphQL","MongoDB", "Docker"],
   }
@@ -19,8 +19,8 @@ export class ProjectService {
     imgUrl: ["projectAssets/projetYuzu/projetYuzuHomeTiny.png", "projectAssets/projetYuzu/projetYuzuArticleTiny.png"],
     name: "projetYuzu",
     shortDescription: "Site d'affichage de news",
-    fullDescription: "Création d'un site d'affichage de news sous Angular 17 avec filtres asynchrones pour s'entrainer sur RxJS. Les articles ont été récupérés via une API externe." +
-      " Ce projet m'a été commandé par Yuzu avant de me prendre en stage.",
+    fullDescription: "Création d'un site d'affichage de news sous Angular 17 avec filtres asynchrones pour s'entrainer sur RxJS. <br>Les articles ont été récupérés via une API externe.<br>" +
+      " Ce projet m'a été commandé par Yuzu en tant que test technique an vue d'une admission en stage de fin de cursus.",
     technologies: ["Angular 17", "TypeScript", "CSS", "API REST", "RxJs"],
     gitHubUrl: "https://github.com/ChrisVai/projetYuzu"
   }
