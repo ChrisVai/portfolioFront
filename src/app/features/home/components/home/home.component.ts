527b8cf7ab8e5c6b95afe0619a38825c354c3e3b
ChrisVai <chrisbellet@gmail.com>
2026-01-23 12:40:07 +0100

    [inProgress] Home : Adding Animations

--- src/app/features/home/components/home/home.component.ts
+++ src/app/features/home/components/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
 import {OutlinedButtonComponent} from "../../../../shared/components/outlined-button/outlined-button.component";
 import {ButtonContentModel} from "../../../../shared/models/buttonContentModel";
+import { gsap } from "gsap"
 
 @Component({
     selector: 'app-home',
@@ -12,10 +13,31 @@ import {ButtonContentModel} from "../../../../shared/models/buttonContentModel";
     templateUrl: './home.component.html',
     styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit{
+
   btnContent: ButtonContentModel = {
     title: "A propos de moi",
     path: "/about",
     routerLinkActive: false
+  }
+
+  ngAfterViewInit(): void {
+    let tl= gsap.timeline()
+    tl.fromTo("h1", {duration: 1, x: 500}, {duration: 1, x: 0})
+    tl.fromTo("h2", {duration: 1, y: 500}, {duration: 1, y: 0})
+    tl.from("app-outlined-button", {duration: 1, opacity: 0})
+
+    gsap.set(".ball", {xPercent: -100, yPercent: -100});
+
+    window.addEventListener("mousemove", e => {
+      gsap.to(".ball", {
+        duration: 0.35,
+        x: e.pageX,
+        y: e.pageY,
+        ease: "none",
+        overwrite: "auto",
+        stagger: 0.035,
+      });
+    });
   }
 }
